9b03983c87c7426b6a1a5d556f0b173a9d795cba
Alpha Lencho <Alpha.lencho-ug@aau.edu.et>
2025-08-26 17:02:02 +0300

    Audio transcription example

--- code/lesson1_openai_sdk/audio_transcription.py
+++ code/lesson1_openai_sdk/audio_transcription.py
@@ -8,7 +8,7 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # Step 2: Transcribe an audio file
-with open("/home/alpha-lencho/ready-tensor/rt-agentic-ai-cert-module4-section1/data/meeting_clip.mp3", "rb") as audio_file:
+with open("code/lesson1_openai_sdk/data/meeting_clip.mp3", "rb") as audio_file:
     transcript = client.audio.transcriptions.create(
         model="whisper-1",
         file=audio_file
